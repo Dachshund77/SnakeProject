5407fcd731e342460355d4d61cc31f9adee82b61
MattyxD19 <mathiasbc97@gmail.com>
2019-04-29 13:23:21 +0200

    test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    Label labelScore;
+    @FXML
+    Button newGame;
+    @FXML
+    Canvas canvas;
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public void handleNewGame(ActionEvent event) {
+
+
+
+    }
 }
