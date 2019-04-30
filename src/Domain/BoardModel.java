660d6fdbf3e566975bd6d771987e76077a37340d
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-01 00:45:13 +0200

    created a simple BoardModel stub

---
+++ src/Domain/BoardModel.java
@@ -0,0 +1,12 @@
+package Domain;
+
+import java.util.ArrayList;
+
+public class BoardModel {
+
+    Double[][] coordinates;
+    ArrayList<Food> foods;
+
+    SnakeHead snakeHead;
+    ArrayList<SnakeBody> snakeBodies;
+}
