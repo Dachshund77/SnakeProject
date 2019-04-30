826c3f06da751f6507b48f8927650fe85654a898
MattyxD19 <mathiasbc97@gmail.com>
2019-04-30 12:31:36 +0200

    Add movement scnd commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,19 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addKeyListener;
 
 
 public class Controller {
@@ -24,16 +31,24 @@ public class Controller {
     public void initialize(){
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.strokeOval(25,25,25,25);
-        gc.strokeOval( 25,50,25,25);
+        gc.strokeOval( posX,posY,25,25);
+
 
 
     }
 
-    public void player(){
+
+
+    public void playerPosition(){
         this.posX = 25;
-        this.posY = 25;
+        this.posY = 50;
 
     }
+
+
+
+
+
 
 
 
--- src/sample/Movement.java
+++ src/sample/Movement.java
@@ -1,21 +1,57 @@
 package sample;
 
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Movement extends Controller {
-   public void keyType(KeyEvent event){
-   if (event.getKeyCode() == KeyEvent.VK_UP){
-      posY++;
-   }
-   if (event.getKeyCode() == KeyEvent.VK_DOWN){
-      posY--;
-   }
-   if (event.getKeyCode() == KeyEvent.VK_RIGHT){
-      posX++;
-   }
-   if (event.getKeyCode() == KeyEvent.VK_LEFT){
-      posX--;
+public class Movement extends Controller implements KeyListener {
+
+   public enum Direction{
+      UP,
+      DOWN,
+      LEFT,
+      RIGHT
    }
 
+   @Override
+   public void keyTyped(KeyEvent e) {
+      if (e.getKeyCode() == KeyEvent.VK_UP){
+         posY++;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_DOWN){
+         posY--;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+         posX++;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_LEFT){
+         posX--;
+      }
+   }
+
+   @Override
+   public void keyPressed(KeyEvent e) {
+      if (e.getKeyCode() == KeyEvent.VK_UP){
+         posY++;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_DOWN){
+         posY--;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+         posX++;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_LEFT){
+
+      }
+   }
+
+   @Override
+   public void keyReleased(KeyEvent e) {
+
+      if (e.getKeyCode() == KeyEvent.VK_UP){
+         posX = posX;
+      }
+      if (e.getKeyCode() == KeyEvent.VK_DOWN){
+
+      }
    }
 }
