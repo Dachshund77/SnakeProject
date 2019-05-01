508a0dded779b76631cedb42f02f31c717db0bb3
jespersen-anders@toft23.dk <Conputer1603!>
2019-05-01 21:25:10 +0200

    Added focus request, fix for key event

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -12,8 +12,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        Scene Scene = new Scene(root,600,600);
+        primaryStage.setScene(Scene);
         primaryStage.show();
+        Scene.getRoot().requestFocus();
     }
 
 
