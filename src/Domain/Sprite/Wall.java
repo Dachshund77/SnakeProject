4d4ad6ddbb2a59e5e2851584d5ba16786724a07f
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-07 16:42:39 +0200

    added wall Class

---
+++ src/Domain/Sprite/Wall.java
@@ -0,0 +1,25 @@
+package Domain.Sprite;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Wall extends Sprite {
+
+    private Color paint;
+
+    public Wall(double xPosition, double yPosition, double height, double width, Color paint) {
+        super(xPosition, yPosition, height, width);
+        this.paint = paint;
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.setFill(paint);
+        gc.rect(xPosition,yPosition,width,height);
+    }
+
+    @Override
+    public void setPaint(Color paint) {
+        this.paint = paint;
+    }
+}
