ed8dcb935bc19adc3f6964e19f2ea16e39273fd8
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-08 21:59:19 +0200

    Refactored MoveablePlayerEntity abstract

--- src/Domain/PlayerEntity/MoveablePlayerEntity.java
+++ src/Domain/PlayerEntity/MoveablePlayerEntity.java
@@ -1,13 +1,37 @@
 package Domain.PlayerEntity;
 
+import Domain.Game.GameModels;
 import Domain.Moveable.Moveable;
+import Domain.Moveable.Moveables;
+import Domain.Sprite.Sprites;
+import javafx.geometry.Rectangle2D;
 
-public abstract class MoveablePlayerEntity extends Moveable implements PlayerEntities  {//TODO need javaDoc
+public abstract class MoveablePlayerEntity implements PlayerEntities, Moveables, Sprites {//TODO need javaDoc
 
+    //From PlayerEnteties interface
     String name;
 
+    //From interface Movables
+    protected double xVelocity;
+    protected double yVelocity;
+    protected double speed;
+
+    // From interface Sprites
+    protected double xPosition;
+    protected double yPosition;
+    protected double height;
+    protected double width;
+    protected boolean isRemoved;
+
     public MoveablePlayerEntity(double xPosition, double yPosition, double height, double width, double speed, String name) {
-        super(xPosition, yPosition, height, width, speed);
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.height = height;
+        this.width = width;
+        this.isRemoved = false;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+        this.speed = speed;
         this.name = name;
     }
 
@@ -17,5 +41,31 @@ public abstract class MoveablePlayerEntity extends Moveable implements PlayerEnt
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public void move(long time, GameModels gameModels) {
+        xPosition += xVelocity * time;
+        yPosition += yVelocity * time;
+    }
+
+    @Override
+    public boolean intersects(Sprites s) {
+        return s.getBoundary().intersects(this.getBoundary());
+    }
+
+    @Override
+    public Rectangle2D getBoundary() {
+        return new Rectangle2D(xPosition, yPosition, width, height);
+    }
+
+    @Override
+    public boolean isRemoved() {
+        return isRemoved;
+    }
+
+    @Override
+    public void setIsRemoved(boolean newStatus) {
+        isRemoved = newStatus;
     }
 }
