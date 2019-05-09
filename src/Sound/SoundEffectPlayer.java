61b360f67ab47f96866ccb67f76e8a08e4d75424
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-09 19:24:13 +0200

    added a simple MediaPlayer

---
+++ src/Sound/SoundEffectPlayer.java
@@ -0,0 +1,55 @@
+package Sound;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+/**
+ * Simple MediaPlayer That can plat Background music (indefinitely) or once a soundEffect.
+ */
+public class SoundEffectPlayer {
+
+    private static MediaPlayer backGroundMusic;
+
+    private static SimpleDoubleProperty soundEffectVolume = new SimpleDoubleProperty(1);
+    private static SimpleDoubleProperty backgroundMusicVolume = new SimpleDoubleProperty(1);
+
+    /**
+     * Plays a MediaFile once. Disposed the MediaPlayer after one Cycle.
+     * @param file The File to be played.
+     */
+    public static void playSoundEffect(File file){
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.volumeProperty().bind(soundEffectVolume);
+        mediaPlayer.setOnEndOfMedia(mediaPlayer::dispose); //Might need a kill all or something
+    }
+
+    /**
+     * Plays a MediaFile as background music.
+     * @param file The File to be played.
+     */
+    public static void playBackGroundMusic(File file){ //If there is ever a problem with suddenly stopping, use a MediaView
+        if (backGroundMusic != null){
+            backGroundMusic.dispose();
+        }
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.volumeProperty().bind(backgroundMusicVolume);
+
+        backGroundMusic = mediaPlayer;
+    }
+
+    /**
+     * Disposed the backgroundMedia Player. This will make the music stop.
+     */
+    public static void disposeBackgroundMusic(){
+        if (backGroundMusic != null){
+            backGroundMusic.dispose();
+        }
+        backGroundMusic = null;
+    }
+}
